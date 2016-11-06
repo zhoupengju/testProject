baf9690f2e11293426cf71d53346e8b285f0352b
周鹏钜 <1321932448@qq.com>
2016-11-06 16:10:48 +0800

    添加图片

--- testProject/testProject/ViewController.m
+++ testProject/testProject/ViewController.m
@@ -42,17 +42,45 @@
 //    NSData *data = UIImageJPEGRepresentation(image, 1);
 //    [data writeToFile:@"" atomically:YES];
     
-    UIImage *image = [UIImage imageNamed:@"2.jpg"];
-    UIGraphicsBeginImageContextWithOptions(image.size, YES, 0);
-    [image drawAtPoint:CGPointMake(0, 0)];
-    UIImage *image1 = [UIImage imageNamed:@"vip_ic_reason_1"];
-    [image1 drawAtPoint:CGPointMake(200, 200)];
-    NSString *str = @"我是超人";
-    [str drawAtPoint:CGPointMake(120, 120) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
-    UIImage *image11 = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-    NSData *data = UIImageJPEGRepresentation(image11, 1);
-    [data writeToFile:@"/Users/zhoupengju/Desktop/test@.png" atomically:YES];
+//    UIImage *image = [UIImage imageNamed:@"2.jpg"];
+//    UIGraphicsBeginImageContextWithOptions(image.size, YES, 0);
+//    [image drawAtPoint:CGPointMake(0, 0)];
+//    UIImage *image1 = [UIImage imageNamed:@"vip_ic_reason_1"];
+//    [image1 drawAtPoint:CGPointMake(200, 200)];
+//    NSString *str = @"我是超人";
+//    [str drawAtPoint:CGPointMake(120, 120) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
+//    UIImage *image11 = UIGraphicsGetImageFromCurrentImageContext();
+//        UIGraphicsEndImageContext();
+//    NSData *data = UIImageJPEGRepresentation(image11, 1);
+//    [data writeToFile:@"/Users/zhoupengju/Desktop/test@.png" atomically:YES];
+    
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 175, 50)];
+    [self.view addSubview:btn];
+
+    btn.backgroundColor = [UIColor orangeColor];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        
+        NSLog(@"监听到的RunLoop状态的改变---%zd", activity);
+    });
+    
+    CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+    
+    // 凡是CF开头的函数创建出来的对象, 都要释放
+    CFRelease(observer);
+    
+}
+
+- (void)btnClick {
+
+    NSLog(@"********************************");
+    
+    UIImageView *imageView = [[UIImageView alloc] init];
+    
+    [imageView performSelector:@selector(setImage:) withObject:[UIImage imageNamed:@"2.png"] afterDelay:3.0 inModes:@[NSDefaultRunLoopMode]];
 }
 
 @end
